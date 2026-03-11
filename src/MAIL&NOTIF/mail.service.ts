cfae57562ef967ea3dfc2b34a4c32b179103b830
Abdoul wahab <applemarket@awahab.local>
2026-03-11 09:00:07 +0100

    correction

--- src/MAIL&NOTIF/mail.service.ts
+++ src/MAIL&NOTIF/mail.service.ts
@@ -8,8 +8,8 @@ export class mailService {
   constructor() {
     this.transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
-      port: parseInt(process.env.MAIL_PORT ?? '465', 10),
-      secure: true,
+      port: parseInt(process.env.MAIL_PORT ?? '587', 10),
+      secure: false,
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
