cb2e25a684d2620c4c2b5147e4770ffe64438e56
Abdoul wahab <applemarket@awahab.local>
2026-03-25 11:30:57 +0100

    profonde amelioration

--- src/favorites/favorites.controller.ts
+++ src/favorites/favorites.controller.ts
@@ -1,8 +1,9 @@
 import { Controller, Get, Post, Param, Body } from '@nestjs/common';
 import { FavoritesService } from './favorites.service';
-import { ApiTags } from '@nestjs/swagger';
 import { IsNumber } from 'class-validator';
 import { Type } from 'class-transformer';
+import { Auth } from 'src/auth/decorators/auth.decorators';
+import { AuthType } from 'src/auth/enums/auth.types.enum';
 
 export class ToggleFavoriteDto {
   @Type(() => Number)
@@ -14,26 +15,29 @@ export class ToggleFavoriteDto {
   produitId: number;
 }
 
-@ApiTags('favorites')
 @Controller('favorites')
 export class FavoritesController {
   constructor(private readonly favoritesService: FavoritesService) {}
 
+  @Auth(AuthType.Bearer)
   @Post('toggle')
   toggle(@Body() dto: ToggleFavoriteDto) {
     return this.favoritesService.toggle(dto.userId, dto.produitId);
   }
 
+  @Auth(AuthType.Bearer)
   @Get('user/:userId')
   findByUser(@Param('userId') userId: number) {
     return this.favoritesService.findByUser(+userId);
   }
 
+  @Auth(AuthType.Bearer)
   @Get('user/:userId/ids')
   getUserFavoriteIds(@Param('userId') userId: number) {
     return this.favoritesService.getUserFavoriteIds(+userId);
   }
 
+  @Auth(AuthType.Bearer)
   @Get('check/:userId/:produitId')
   isFavorite(@Param('userId') userId: number, @Param('produitId') produitId: number) {
     return this.favoritesService.isFavorite(+userId, +produitId);
